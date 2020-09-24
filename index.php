e01b3f142bd1061030c6ee4f802f7a0686bd0c49
TabulaWeb <67907195+TabulaWeb@users.noreply.github.com>
2020-09-24 12:10:10 +0300

    ERROR

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ if ($_SERVER['REQUEST_URI'] == "/"){
 }
 include 'core/libcountry.php';
 include 'Pages/MetaData.php';
+include 'Pages/BasePage.php';
 
 $include_path = '';
 
@@ -68,6 +69,8 @@ if(urldecode($_SERVER['REQUEST_URI']) == "/"){
 	header("HTTP/1.0 404 Not Found");
 	$include_path = '404.html';
   }
+
+
 // ===================================================================== //
 ?>
 <!DOCTYPE html>
@@ -106,40 +109,13 @@ echo $bodyup;
 <body itemscope='itemscope' itemtype='http://schema.org/WebPageElement'> 
 <?php echo $bodydown;?>
 
-	<nav class='navbar navbar-inverse navbar-fixed-top' itemscope='' itemtype='http://www.schema.org/SiteNavigationElement'>
-      <div class='container'>
-        <div class='navbar-header'>
-          <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
-            <span class='icon-bar'></span>
-            <span class='icon-bar'></span>
-            <span class='icon-bar'></span>
-          </button>
-		<a class='navbar-brand' href='/'><span style='color: #f3e99a;'>all-</span>populations<span style='color: #eee;'>.com</span></a>
-        </div>
-        <div class='navbar-collapse collapse'>
-          <ul class='nav navbar-nav navbar-left'>
-            <li itemprop='name'><a itemprop='url' href='../ru/list-of-countries-by-population.html'>Страны</a></li>
-            <li itemprop='name'><a itemprop='url' href='../ru/list-of-kingdoms-by-population.html'>Королевства</a></li>
-            <li itemprop='name'><a itemprop='url' href='../ru/list-of-republics-by-population.html'>Республики</a></li>
-            <li itemprop='name'><a itemprop='url' href='../ru/list-of-cities-by-population.html'>Города</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+	<?php BaseHeader();?>
 	
 	<?php  
 	include $include_path;
 	?>
-	<footer itemscope='itemscope' itemtype='http://schema.org/WPFooter'>
-		<?php echo $footerup;?>
-			<div class='container'>
-				<div class='row centered' itemprop='text'>
-					<p>© All-populations.com - <?php echo $year;?></p>
-					<p>Узнайте численность населения в вашем регионе!</p>
-				</div>
-			</div>
-		<?php echo $footerdown;?>
-	</footer>
+
+	<?php BaseFooter();?>
 	
     <script src='https://code.jquery.com/jquery-1.10.2.min.js'></script>
     <script async src='/assets/js/bootstrap.min.js'></script>
